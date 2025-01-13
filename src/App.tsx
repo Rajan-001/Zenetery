ed779c70bb4fd08711f8bae486dcb74671fb35fe
Rajan-001 <81520943+Rajan-001@users.noreply.github.com>
2025-01-14 04:11:52 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+import "./App.css"
+import { Hero } from "./component/Hero"
+
+function App() {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+    </main>
+  )
+}
+
+export default App
---
+++ src/component/Button.tsx
@@ -0,0 +1,15 @@
+export function Button({ title, id, rightIcon, leftIcon, containerClass }) {
+  return (
+    <button
+      id={id}
+      className={`group relative x-10 w-fit cursor-pointer overflow-hidden rounded-full  px-7 py-3 text-black
+        ${containerClass}
+        `}
+    >
+      {leftIcon}
+      <span className="relative incline-flex overflow-hidden font-general text-xs upperCase">
+        <div>{title}</div>
+      </span>
+    </button>
+  )
+}
